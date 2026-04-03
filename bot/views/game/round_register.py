a2cb9ceaa586450a51a4f4dc2cf3f27ef8d9bc95
Nek0Anim3 <pogoderopa@gmail.com>
2026-04-03 19:00:54 +0300

    Return to list["RoundView"], need separately

--- bot/views/game/round_register.py
+++ bot/views/game/round_register.py
@@ -1,11 +1,13 @@
 from bot.views.game.round_menu import RoundView
 
-active_rounds: dict[int, "RoundView"] = {}
+active_rounds: dict[int, list["RoundView"]] = {}
 
 def register_round_view(lobby_id: int, round_view: RoundView):
     if lobby_id not in active_rounds:
-        active_rounds[lobby_id] = round_view
+        active_rounds[lobby_id] = [round_view]
         print(f"ROUND REGISTER: Registered round with lobby_id {lobby_id}")
+    else:
+        active_rounds[lobby_id].append(round_view)
 
 def get_round_by_lobby_id(lobby_id: int):
     if lobby_id not in active_rounds:
